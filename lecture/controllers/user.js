1161e1b0ab8d5672a33b26a78b8b1c1ee6f77d05
jiheon2234 <jiheon2234@gmail.com>
2024-01-20 11:36:31 +0900

    팔로잉 끊기

--- lecture/controllers/user.js
+++ lecture/controllers/user.js
@@ -7,11 +7,20 @@ exports.follow = async (req, res, next) => {
         if (user) {
             user.addFollowing(parseInt(req.params.id));
             res.send('success');
-        }else{
+        } else {
             res.status(404).send('no user');
         }
     } catch (e) {
         console.error(e);
         next(e);
     }
+};
+
+exports.unfollow = async (req, res, next) => {
+    User.findByPk(req.params.id)
+        .then((user) => {
+            user.removeFollowing(parseInt(req.user.id));
+            res.send('success');
+        })
+        .catch(e => next(e));
 };
--- lecture/routes/user.js
+++ lecture/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const {isLoggedIn} = require('../middlewears');
-const {follow} = require('../controllers/user')
+const {follow, unfollow} = require('../controllers/user')
 
 const router = express.Router();
 
 router.post('/:id/follow',isLoggedIn, follow);
+router.post('/:id/unfollow',isLoggedIn, unfollow)
 
 module.exports = router;
