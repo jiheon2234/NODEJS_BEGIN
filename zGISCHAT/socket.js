ede4e29be01bbbe2eed8578e7d59a388ca4fc3de
jiheon2234 <jiheon2234@gmail.com>
2024-01-25 22:19:04 +0900

    socket.io

--- zGISCHAT/socket.js
+++ zGISCHAT/socket.js
@@ -1,27 +1,31 @@
-const Websocket = require('ws');
+const SocketIo = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new Websocket.Server({server});
+    const io = SocketIo(server, {path : '/socket.io'})
 
-    wss.on('connection', (ws, req) => {
+    io.on('connection', (socket) => {
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
+        console.log('새로운 클라이언트 접속', socket.id, req.ip);
+        socket.on('disconnect', ()=>{
+            console.log('클라이언트 접속 해제', ip, socket.id, req.ip);
+            clearInterval(socket.interval);
+        })
 
-        ws.on('message', (message) => {
+        io.on('message', (message) => {
             console.log((message.toString()));
 
         });
 
-        ws.on('error', console.error);
-        ws.on('close', () => {
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
-        });
+        io.on('error', console.error);
 
-        ws.interval = setInterval(() => {
-            if (ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메시지를 보냄');
-            }
+        io.on('reply',(data)=>{
+            console.log(data);
+        })
+
+
+        socket.interval = setInterval(() => {
+                socket.emit('news','Hello Socket.IO');
         }, 3000);
     });
 };
