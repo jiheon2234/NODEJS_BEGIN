843271f1105c8509776bdd010c63d5bea1c0cbb1
jiheon2234 <jiheon2234@gmail.com>
2024-01-26 23:05:59 +0900

    기본 채팅방

--- zGISCHAT/app.js
+++ zGISCHAT/app.js
@@ -1,41 +1,60 @@
-const express =require('express')
-const path = require('path')
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const nunjucks = require('nunjucks')
-const dotenv = require('dotenv')
-dotenv.config();
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const ColorHash = require('color-hash').default;
+const dotenv = require('dotenv');
 
-const webSocket = require('./socket')
+dotenv.config();
+const connect = require('./schemas');
+const webSocket = require('./socket');
 const indexRouter = require('./routes');
-const app = express()
-app.set('port',process.env.PORT || 8005);
+const app = express();
+app.set('port', process.env.PORT || 8005);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
     watch: true,
-})
+});
+connect();
 
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.urlencoded({extended : false}))
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(express.json())
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.json());
 
 app.use(session({
     resave: false,
-    saveUninitialized : false,
+    saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
-    cookie : {
-        httpOnly : true,
-        secure : false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
     }
-}))
+}));
 
-const server = app.listen(app.get('port'), ()=>{
-    console.log(app.get('port'),'번 포트에서 대기중');
+app.use((req, res, next) => {
+    if (!req.session.color) {
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+        console.log(req,session.color, req.sessionID)
+    }
+    next();
+});
+
+app.use((err,req,res,next)=>{
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500)
+    res.render('error')
 })
-webSocket(server)
 
-app.use('/',indexRouter);
+const server = app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
+webSocket(server, app);
+
+app.use('/', indexRouter);
--- zGISCHAT/socket.js
+++ zGISCHAT/socket.js
@@ -1,31 +1,30 @@
 const SocketIo = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app) => {
     const io = SocketIo(server, {path : '/socket.io'})
+    app.set('io',io)
 
-    io.on('connection', (socket) => {
-        const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        console.log('새로운 클라이언트 접속', socket.id, req.ip);
-        socket.on('disconnect', ()=>{
-            console.log('클라이언트 접속 해제', ip, socket.id, req.ip);
-            clearInterval(socket.interval);
+    const room = io.of('/room');
+    const chat = io.of('/chat');
+
+    room.on('connection', (socket) =>{
+        console.log('room 네임스페이스 접속')
+
+        socket.on('disconnect',()=>{
+            console.log('room 네임스페이스 접속 해제');
+        })
+    })
+
+    chat.on('connection', (socket) =>{
+        console.log('chat 네임스페이스 접속')
+
+        socket.on('join', (data)=>{
+            socket.join(data); // 방에 참여
+            socket.leave(data)
         })
 
-        io.on('message', (message) => {
-            console.log((message.toString()));
-
-        });
-
-        io.on('error', console.error);
-
-        io.on('reply',(data)=>{
-            console.log(data);
+        socket.on('disconnect',()=>{
+            console.log('chat 네임스페이스 접속 해제');
         })
-
-
-        socket.interval = setInterval(() => {
-                socket.emit('news','Hello Socket.IO');
-        }, 3000);
-    });
+    })
 };
